da969af87b7c8fdcb643ce0c9ab0d193dd81faf9
Luis Cabellos <zhen.sydow@gmail.com>
2013-01-27 15:59:49 +0100

    triangle

--- main.cpp
+++ main.cpp
@@ -1,18 +1,56 @@
+//------------------------------------------------------------------------------
 #include <cstdlib>
+
+#define GL_GLEXT_PROTOTYPES 1
+#define GL3_PROTOTYPES 1
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glext.h>
 #include <GL/glut.h>
 
-void renderScene(void) {
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+//------------------------------------------------------------------------------
+const float vertexPositions[] = {
+  0.75f, 0.75f, 0.0f, 1.0f,
+  0.75f, -0.75f, 0.0f, 1.0f,
+  -0.75f, -0.75f, 0.0f, 1.0f,
+};
 
-  glBegin(GL_TRIANGLES);
-  glVertex3f(-0.5,-0.5,0.0);
-  glVertex3f(0.5,0.0,0.0);
-  glVertex3f(0.0,0.5,0.0);
-  glEnd();
+GLuint positionBufferObject = 0;
+GLuint myProgram = 0;
+
+//------------------------------------------------------------------------------
+void initializeVertexBuffer(){
+  glGenBuffers( 1, &positionBufferObject );
+
+  glBindBuffer( GL_ARRAY_BUFFER, positionBufferObject );
+  glBufferData( GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions,
+		GL_STATIC_DRAW );
+  glBindBuffer( GL_ARRAY_BUFFER, 0 );
+}
+
+//------------------------------------------------------------------------------
+void renderScene(void) {
+  glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
+  glClear( GL_COLOR_BUFFER_BIT );
+
+  glUseProgram( myProgram );
+  glBindBuffer( GL_ARRAY_BUFFER, positionBufferObject );
+  glEnableVertexAttribArray( 0 );
+  glVertexAttribPointer( 0, 4, GL_FLOAT, GL_FALSE, 0, 0 );
+
+  glDrawArrays( GL_TRIANGLES, 0, 3 );
+
+  glDisableVertexAttribArray( 0 );
+  glUseProgram( 0 );
 
   glutSwapBuffers();
 }
 
+void reshape( int w, int h ){
+    glViewport( 0, 0, (GLsizei)w, (GLsizei)h );
+}
+
+//------------------------------------------------------------------------------
 int main(int argc, char **argv ){
   // init GLUT and create Window
   glutInit( &argc, argv );
@@ -20,10 +58,15 @@ int main(int argc, char **argv ){
   glutInitWindowSize( 320, 320 );
   glutCreateWindow( "Onion World" );
 
+  initializeVertexBuffer();
+
   // register callbacks
-  glutDisplayFunc(renderScene);
+  glutDisplayFunc( renderScene );
+  glutReshapeFunc( reshape );
 
   glutMainLoop();
 
   return EXIT_SUCCESS;
 }
+
+//------------------------------------------------------------------------------
