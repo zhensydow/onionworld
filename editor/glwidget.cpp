bea9079a8c351ba99ff265b31331a3b4469bd3fa
Luis Cabellos <zhen.sydow@gmail.com>
2013-02-02 16:30:15 +0100

    add basic GL widget

--- editor/glwidget.cpp
+++ editor/glwidget.cpp
@@ -1,20 +1,9 @@
 //------------------------------------------------------------------------------
-#include <cstdlib>
-
-#include <QtGui/QApplication>
-
 #include "glwidget.h"
 
 //------------------------------------------------------------------------------
-int main(int argc, char **argv ){
-  QApplication app(argc, argv);
-
-  GLWidget window;
-
-  window.resize( 320, 320 );
-  window.show();
-
-  return app.exec();
+GLWidget::GLWidget(QWidget *parent) : QGLWidget(parent) {
+  setMouseTracking( true );
 }
 
 //------------------------------------------------------------------------------
