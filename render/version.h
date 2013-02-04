266c56fdd4327935cd01cc26e78df9335696190c
Luis Cabellos <cabellos@ifca.unican.es>
2013-02-04 13:04:57 +0100

    add version copy constructor

--- render/version.h
+++ render/version.h
@@ -9,8 +9,10 @@
 namespace Render{
   class Version{
   public:
+    Version( unsigned int ma = 0, unsigned int mi = 0 );
     Version( const std::string &versionStr );
-    Version( unsigned int ma, unsigned int mi );
+
+    Version( const Version& v);
 
     bool operator == (const Version & v) const;
     bool operator != (const Version & v) const;
@@ -22,6 +24,11 @@ namespace Render{
   inline Version::Version( const unsigned int ma, const unsigned int mi ){
       this->mayor = ma;
       this->minor = mi;
+  }
+
+  inline Version::Version( const Version & v ){
+      this->mayor = v.mayor;
+      this->minor = v.minor;
   }
 
   inline bool Version::operator ==(const Version & v) const{
