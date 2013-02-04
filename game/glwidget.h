da68664dc34be88fde71ab89459a3e14f3589c32
Luis Cabellos <cabellos@ifca.unican.es>
2013-02-04 17:31:11 +0100

    remove glut, use QT on game

---
+++ game/glwidget.h
@@ -0,0 +1,30 @@
+//------------------------------------------------------------------------------
+#ifndef GLWIDGET_H
+#define GLWIDGET_H
+
+//------------------------------------------------------------------------------
+#include <QtOpenGL/QGLWidget>
+
+namespace Render{
+  class Render;
+}
+
+//------------------------------------------------------------------------------
+class GLWidget : public QGLWidget {
+ public:
+  GLWidget(QWidget *parent = NULL);
+  void initializeGL();
+  void resizeGL(int w, int h);
+  void paintGL();
+
+ protected:
+
+ private:
+  Q_OBJECT
+	Render::Render * render;
+};
+
+//------------------------------------------------------------------------------
+#endif//GLWIDGET_H
+
+//------------------------------------------------------------------------------
--- game/main.cpp
+++ game/main.cpp
@@ -1,48 +1,20 @@
 //------------------------------------------------------------------------------
 #include <cstdlib>
 
-#define GL_GLEXT_PROTOTYPES 1
-#define GL3_PROTOTYPES 1
-#include <GL/gl.h>
-#include <GL/glu.h>
-#include <GL/glext.h>
-#include <GL/glut.h>
+#include <QtGui/QApplication>
 
-#include "render/render.h"
-
-//------------------------------------------------------------------------------
-Render::Render * render = 0;
-
-//------------------------------------------------------------------------------
-void renderScene(void) {
-    render->renderScene();
-    glutSwapBuffers();
-}
-
-//------------------------------------------------------------------------------
-void resize( int w, int h ){
-  render->resize( w, h );
-}
+#include "game/glwidget.h"
 
 //------------------------------------------------------------------------------
 int main(int argc, char **argv ){
-  render = Render::Render::instance();
+  QApplication app(argc, argv);
 
-  // init GLUT and create Window
-  glutInit( &argc, argv );
-  glutInitDisplayMode( GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA );
-  glutInitWindowSize( 320, 320 );
-  glutCreateWindow( "Onion World" );
+  GLWidget window;
 
-  render->initialize();
+  window.resize( 320, 320 );
+  window.show();
 
-  // register callbacks
-  glutDisplayFunc( renderScene );
-  glutReshapeFunc( resize );
-
-  glutMainLoop();
-
-  return EXIT_SUCCESS;
+  return app.exec();
 }
 
 //------------------------------------------------------------------------------
