cafcd46fff552a3f7182db9843d5264ce91503fe
Luis Cabellos <zhen.sydow@gmail.com>
2013-02-14 18:48:36 +0100

    use simplify1

--- test/test02.cpp
+++ test/test02.cpp
@@ -3,16 +3,23 @@
 #include <iostream>
 
 #include "math/QBezier.h"
+#include "math/CBezier.h"
 
 //------------------------------------------------------------------------------
 int main(int argc, char **argv ){
     Math::QBezier qb{{0,0},{10,10},{20,0}};
+    Math::CBezier cb{{0,0},{8,8},{15,10},{20,0}};
 
-    for( int i = 0 ; i < 101 ; ++i ){
-        auto v = qb.eval(i / 100.0);
-        std::cout << i << ": " << v.x << "," << v.y << std::endl;
-    }
+    auto qbs = cb.simplify1();
 
+    std::cout << "[" << qbs.p0.x << ", " << qbs.p0.y << "] ";
+    std::cout << "[" << qbs.p1.x << ", " << qbs.p1.y << "] ";
+    std::cout << "[" << qbs.p2.x << ", " << qbs.p2.y << "] ";
+    std::cout << std::endl;
+
+    auto ev1 = cb.eval(0.5);
+    auto ev2 = qbs.eval(0.5);
+    std::cout << ev1.x << "," << ev1.y << "=" << ev2.x << "," << ev2.y << std::endl;
     return EXIT_SUCCESS;
 }
 
