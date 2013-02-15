0e46ba7745976e37211209e399465989c9d727fc
Luis Cabellos <cabellos@ifca.unican.es>
2013-02-15 17:17:09 +0100

    add oassert without Qt

--- common/debug.h
+++ common/debug.h
@@ -1,12 +1,15 @@
+//------------------------------------------------------------------------------
 #ifndef COMMON_DEBUG_H
 #define COMMON_DEBUG_H
 
-#include <QMessageBox>
-
+//------------------------------------------------------------------------------
 #define str(A) #A
-#define xstr(A) str(a)
+#define xstr(A) str(A)
 
+//------------------------------------------------------------------------------
 #if defined(DEBUG)
+#if defined(QT_VERSION)
+#include <QMessageBox>
 #define oassert(EXPR) {                                                 \
         static bool ignore = false;                                     \
         if( !ignore && !(EXPR) ){                                       \
@@ -24,10 +27,38 @@
             }                                                           \
         }                                                               \
     }
+#else
+#include <iostream>
+#define oassert(EXPR) {                                                 \
+        static bool ignore = false;                                     \
+        if( !ignore && !(EXPR) ){                                       \
+            std::cout << "Assert: " #EXPR << std::endl;                 \
+            std::cout << __FILE__ ":" xstr(__LINE__) << std::endl;      \
+            std::cout << "(b)reak, (i)gnore, ignore (a)lways? ";        \
+            char ret;                                                   \
+            std::cin >> ret;                                            \
+            switch(ret){                                                \
+            case 'b':                                                   \
+            case 'B':                                                   \
+                exit( EXIT_FAILURE ); break;                            \
+            case 'a':                                                   \
+            case 'A':                                                   \
+                ignore = true; break;                                   \
+            }                                                           \
+        }                                                               \
+    }
+#endif//QT_VERSION
+
+//------------------------------------------------------------------------------
 #elif defined(RELEASE)
 #define oassert(EXPR)
+
+//------------------------------------------------------------------------------
 #else
 #error Unknown Build Type
 #endif
 
+//------------------------------------------------------------------------------
 #endif//COMMON_DEBUG_H
+
+//------------------------------------------------------------------------------
