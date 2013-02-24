de1e91185b636b0e645a67cf7529e39006f6def7
Luis Cabellos <zhen.sydow@gmail.com>
2013-02-24 20:50:32 +0100

    add two columns

--- editor/mainwindow.cpp
+++ editor/mainwindow.cpp
@@ -6,37 +6,52 @@
 
 MainWindow::MainWindow(){
     QWidget *widget = new QWidget;
-    setCentralWidget(widget);
+    setCentralWidget( widget );
 
     GLWidget * glw = new GLWidget();
 
-    QVBoxLayout *layout = new QVBoxLayout;
-    layout->setMargin(5);
-    layout->addWidget(glw);
-    
-    widget->setLayout(layout);
+    QHBoxLayout *body = new QHBoxLayout;
+    body->setMargin( 5 );
+
+    QGroupBox *left = new QGroupBox( tr("Edit") );
+    QVBoxLayout *leftl = new QVBoxLayout;
+    left->setLayout( leftl );
+
+    QGroupBox *rigth = new QGroupBox( tr("View") );
+    QVBoxLayout *rigthl = new QVBoxLayout;
+    rigthl->addWidget( glw );
+
+    QGroupBox *cameraBox = new QGroupBox( tr("Camera") );
+    rigthl->addWidget( cameraBox );
+
+    rigth->setLayout( rigthl );
+
+    body->addWidget( left );
+    body->addWidget( rigth );
+
+    widget->setLayout( body );
 
     createActions();
     createMenus();
 }
 
 void MainWindow::createActions(){
-    aboutAct = new QAction(tr("&About"), this);
-    aboutAct->setStatusTip(tr("Show the application's About box"));
-    connect(aboutAct, SIGNAL(triggered()), this, SLOT(about()));
+    aboutAct = new QAction( tr("&About"), this);
+    aboutAct->setStatusTip( tr("Show the application's About box") );
+    connect( aboutAct, SIGNAL(triggered()), this, SLOT(about()) );
 }
 
 void MainWindow::createMenus(){
-    fileMenu = menuBar()->addMenu(tr("&File"));
+    fileMenu = menuBar()->addMenu( tr("&File") );
     fileMenu->addSeparator();
 
-    editMenu = menuBar()->addMenu(tr("&Edit"));
+    editMenu = menuBar()->addMenu( tr("&Edit") );
     editMenu->addSeparator();
 
-    helpMenu = menuBar()->addMenu(tr("&Help"));
+    helpMenu = menuBar()->addMenu( tr("&Help") );
     helpMenu->addAction( aboutAct );
 }
 
 void MainWindow::about(){
-    printf("About\n" );
+    printf( "About\n" );
 }
